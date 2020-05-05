0e41a1c297f8d1d0dd036efe1a35c696c2f8dd56
Ivan Rossi <ivanross.me@gmail.com>
2020-05-05 17:21:51 +0200

    💥 Bang! First commit
    App bootstrapped with create-accurapp

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Your React App goes here!</h2>
+      </div>
+    )
+  }
+}
